114b17a3518ca72aff833eab660f981a4582862a
Soulmate7 <1012194891@qq.com>
2021-06-04 14:13:43 +0800

    git commit -m "delet flask"

--- helloform.py
+++
@@ -1,8 +0,0 @@
-from flask  import Flask,render_template
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
-from wtforms.validators import DataRequired
-
-app=Flask(__name__)
-app.config['SECRET_KEY']=
